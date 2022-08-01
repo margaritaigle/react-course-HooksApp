5f29a0680a95b5d7030d21c6042ded95f7530b37
Margarita Iglesias <margaritaigle99@gmail.com>
2022-07-31 23:11:10 -0300

    a

--- src/01-useState/CounterWithCustomHook.jsx
+++ src/01-useState/CounterWithCustomHook.jsx
@@ -6,13 +6,17 @@ export const CounterWithCustomHook = () => {
 
     return (
         <>
-            <h1>Counter with Hook: {counter}</h1>
+            <h1>Counter with Hook: { counter }</h1>
 
             <hr />
 
-            <button className="btn btn-primary" onClick={increment}>+1</button>
+            <button className="btn btn-primary" onClick={ () => increment(2) }>+2</button>
             <button className="btn btn-primary" onClick={reset}>Reset</button>
-            <button className="btn btn-primary" onClick={decrement}>-1</button>
+            <button className="btn btn-primary" onClick={ () => decrement(2) }>-2</button>
         </>
     )
 }
+
+// (event) => increment(event) === increment 
+// A callback is a function passed as an argument to another function.
+// When you pass a function as an argument, remember not to use parenthesis.
