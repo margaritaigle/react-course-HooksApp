e7d4a53677ad6191ffc321962d58d6aa37627fbb
Margarita Iglesias <margaritaigle99@gmail.com>
2022-08-18 00:19:26 -0300

    .

---
+++ src/08-useReducer/TodoApp.jsx
@@ -0,0 +1,46 @@
+import { useReducer } from "react"
+import { TodoAdd } from "./TodoAdd";
+import { TodoList } from "./TodoList";
+import { todoReducer } from "./todoReducer"
+
+export const TodoApp = () => {
+
+    const initialState = [
+        {
+            id: new Date().getTime(),
+            description: 'Recolectar la piedra del alma',
+            done: false
+        },
+        {
+            id: new Date().getTime() * 3,
+            description: 'Recolectar la piedra del alma',
+            done: false
+        },
+    ]
+
+    const [todos, dispatch] = useReducer(todoReducer, initialState);
+
+    const handleNewTodo = ( todo ) => {
+        console.log( {todo} )
+    }
+    
+    return (
+        <>
+            <h1>TodoApp: 10 <small>Pendientes: 2</small></h1>
+            <hr />
+
+            <div className="row">
+                <div className="col-7">
+                    <TodoList todos={todos} /> 
+                </div>
+
+                <div className="col-5">
+                    <h4>Agregar TODO</h4>
+                    <hr />
+                    <TodoAdd onNewTodo={handleNewTodo}/>
+                </div>
+            </div>
+
+        </>
+    )
+}
