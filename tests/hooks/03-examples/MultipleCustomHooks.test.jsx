fec57d6392bd79048506fef06bb148f21b22265d
Margarita Iglesias <margaritaigle99@gmail.com>
2022-09-08 19:25:50 -0300

    added MultipleCustomHooks tests

---
+++ tests/hooks/03-examples/MultipleCustomHooks.test.jsx
@@ -0,0 +1,70 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { MultipleCustomHooks } from "../../../src/03-examples/MultipleCustomHooks";
+import { useCounter } from "../../../src/hooks/useCounter";
+import { useFetch } from "../../../src/hooks/useFetch";
+
+jest.mock("../../../src/hooks/useCounter");
+jest.mock("../../../src/hooks/useFetch");
+
+describe('Test in <MultipleCustomHooks />', () => {
+    
+    const mockIncrement = jest.fn();
+
+    useCounter.mockReturnValue({
+        counter: 1, 
+        increment: mockIncrement,
+    });
+
+    beforeEach( () => {
+        jest.clearAllMocks();
+    });
+
+    test('should show the default component', () => {
+
+        useFetch.mockReturnValue({
+            data: null,
+            isLoading: true,
+            hasError: null
+        });
+
+        render(<MultipleCustomHooks />);
+
+        expect(screen.getByText('Loading...')).toBeTruthy();
+        expect(screen.getByText('Breaking Bad Quotes')).toBeTruthy();
+
+        const nextButton = screen.getByRole('button', { name: 'Next quote' })
+        expect(nextButton.disabled).toBeTruthy();
+    });
+
+    test('should show a quote', () => {
+
+        useFetch.mockReturnValue({
+            data: [{ author: "Margarita", quote: "May the force be with you" }],
+            isLoading: false,
+            hasError: null
+        });
+
+        render(<MultipleCustomHooks />);
+        expect(screen.getByText("Margarita")).toBeTruthy();
+        expect(screen.getByText("May the force be with you")).toBeTruthy();
+
+        const nextButton = screen.getByRole('button', { name: 'Next quote' })
+        expect(nextButton.disabled).toBeFalsy();
+    });
+
+    test('should call the increment function', () => {
+
+        useFetch.mockReturnValue({
+            data: [{ author: "Margarita", quote: "May the force be with you" }],
+            isLoading: false,
+            hasError: null
+        });
+
+
+        render(<MultipleCustomHooks />);
+        const nextButton = screen.getByRole('button', { name: 'Next quote' });
+        fireEvent.click(nextButton);
+
+        expect(mockIncrement).toHaveBeenCalled()
+    });
+})
--- tests/hooks/useForm.test.js
+++ tests/hooks/useForm.test.js
@@ -1,4 +1,5 @@
 import { renderHook } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
 import { useForm } from "../../src/hooks/useForm";
 describe('Tests in useForm', () => { 
 
@@ -17,5 +18,30 @@ describe('Tests in useForm', () => {
             onResetForm: expect.any( Function )
           }
       )
-     });
- });
+    });
+
+    test('should change the name in the form', () => {
+        const { result } = renderHook(() => useForm(initialForm));
+        const { onInputChange } = result.current;
+
+        act(() => {
+            onInputChange({ target: { name: "name", value: "Francisco" } })
+        });
+
+        expect(result.current.name).toBe("Francisco");
+        expect(result.current.formState.name).toBe("Francisco");
+    });
+
+    test('should reset the values in the form', () => {
+        const { result } = renderHook(() => useForm(initialForm));
+        const { onInputChange, onResetForm } = result.current;
+
+        act(() => {
+            onInputChange({ target: { name: "name", value: "Francisco" } })
+            onResetForm()
+        });
+        
+        expect(result.current.name).toBe(initialForm.name);
+        expect(result.current.formState.name).toBe(initialForm.name);
+    });
+});
