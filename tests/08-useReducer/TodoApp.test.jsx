cf9443e728b4c8c6c6caffa77f918a71162994b3
Margarita Iglesias <margaritaigle99@gmail.com>
2022-09-08 22:12:03 -0300

    testing TodoApp

---
+++ tests/08-useReducer/TodoApp.test.jsx
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+import { TodoApp } from "../../src/08-useReducer/TodoApp";
+import { useTodo } from "../../src/hooks/useTodo";
+
+jest.mock("../../src/hooks/useTodo");
+
+describe('testing <TodoApp />', () => {
+
+    useTodo.mockReturnValue({
+        todos: [
+            {id: 1, description: "Todo#1", done: false},
+            {id: 2, description: "Todo#2", done: true},
+        ],
+        todosCount: 2,
+        pendingTodosCount: 1,
+        handleDeleteTodo: jest.fn(),
+        handleToggleTodo: jest.fn(),
+        handleNewTodo: jest.fn()
+    });
+    test('should show the component correctly', () => {
+
+        render(<TodoApp />)
+
+        expect( screen.getByText('Todo#1') ).toBeTruthy();
+        expect( screen.getByText('Todo#2') ).toBeTruthy();
+        expect( screen.getByRole('textbox') ).toBeTruthy();
+    });
+});
